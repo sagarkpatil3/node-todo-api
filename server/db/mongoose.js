a231d0546c2aa498f33dcc0e484dec9520381a7f
Sagar Patil <sagarkp3@gmail.com>
2019-01-08 18:15:29 +0530

    changes in db configuration

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,12 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://sagarkp3@gmail.com:Sagarkp3@ds237713.mlab.com:37713/mongoose-node' || 'mongodb://localhost:27017/TodoApp');
+
+let db = {
+    localhost: 'mongodb://localhost:27017/TodoApp',
+    mlab: 'mongodb://sagarkp3@gmail.com:Sagarkp3@ds237713.mlab.com:37713/mongoose-node'
+};
+mongoose.connect(db.localhost || db.mlab);
 
 
 module.exports = {
--- server/server.js
+++ server/server.js
@@ -1,65 +1,56 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
-var { mongoose } = require('./db/mongoose');
-var { Todo } = require('./models/todo');
-var { User } = require('./models/user');
-var { ObjectID } = require('mongodb');
-
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
 var app = express();
 const port = process.env.PORT || 3000;
-// MiddleWare
+
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
-    console.log(req.body);
-    var todo = new Todo({
-        text: req.body.text
-    });
+  var todo = new Todo({
+    text: req.body.text
+  });
 
-    todo.save().then((todos)=>{
-        res.status(200).send(todos);
-        // console.log("Successfully inserted -:", JSON.stringify(todos,undefined,2));
-    },(err) => {
-        res.status(400).send(err);
-        // console.log('Error in insertion', err);
-    });
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
 });
 
 app.get('/todos', (req, res) => {
-    console.log(res.body);
-    Todo.find().then( (todos) => {
-        res.send({todos});
-    }, (err) => {
-        res.status(400).send(err);    
-    })
-})
-
-
-
-app.get('/todos/:id',(req,res) => {
-    var id = req.params.id;
-    // res.send(req.params);
-    if(!ObjectID.isValid){
-        res.status(404).send();
-    } else {
-        Todo.findById(id).then( (todo)=> {
-            if(!todo) {
-                return res.status(404).send();
-            }
-            res.send({ todo });
-        }).catch((err) => {
-            res.status(404).send(err);
-        })
-    } 
-})
-
-
-
-app.listen(port, () => {
-    console.log(`Started on port at:- ${port}`);
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  });
 });
 
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
 
-module.exports = { app };
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Started up at port ${port}`);
+});
+
+module.exports = {app};
