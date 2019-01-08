91e551909ae15076d67c6a07bb57b9a84df8bfb0
Sagar Patil <sagarkp3@gmail.com>
2019-01-08 18:37:14 +0530

    bugs fixed in db configuration

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,7 +4,7 @@ mongoose.Promise = global.Promise;
 
 let db = {
     localhost: 'mongodb://localhost:27017/TodoApp',
-    mlab: 'mongodb://sagarkp3@gmail.com:Sagarkp3@ds237713.mlab.com:37713/mongoose-node'
+    mlab: 'mongodb://sagarkp3@gmail.com:Sagarkp3@ds237713.mlab.com:37713/todoapp'
 };
 mongoose.connect(db.localhost || db.mlab);
 
