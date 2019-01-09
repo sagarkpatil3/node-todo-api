49e93befbde6f01ac065f1eb9b20fdc4c8a03374
Sagar Patil <sagarkp3@gmail.com>
2019-01-09 10:11:58 +0530

    added some changes in mongo db js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,7 +4,7 @@ mongoose.Promise = global.Promise;
 
 let db = {
     localhost: 'mongodb://localhost:27017/TodoApp',
-    mlab: process.env.MONGOLAB_URI
+    mlab: process.env.MONGOLAB_URI = 'mongodb://sagarkp3@gmail.com:Sagarkp3@ds237713.mlab.com:37713/todoapp'
 };
 mongoose.connect(db.localhost || db.mlab);
 
