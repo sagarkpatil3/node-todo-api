2061bd220197ecfa9287856b26fd95413eca5e56
Sagar Patil <sagarkp3@gmail.com>
2019-01-16 22:33:55 +0530

    changed in mongoose connection

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -6,7 +6,7 @@ let db = {
     localhost: 'mongodb://localhost:27017/TodoApp',
     mlab: process.env.MONGOLAB_URI = 'mongodb://sagarkp3@gmail.com:Sagarkp3@ds237713.mlab.com:37713/todoapp'
 };
-mongoose.connect(db.localhost || db.mlab);
+mongoose.connect(db.mlab || db.localhost );
 
 
 module.exports = {
