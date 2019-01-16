6ea854f2d2cfa8ef524dca201b80983a8364f59d
Sagar Patil <sagarkp3@gmail.com>
2019-01-16 23:10:02 +0530

    changed in mongoose connection setup and some changes

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,10 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
-module.exports = { mongoose };
+mongoose.connect(process.env.MONGOLAB_URI ||'mongodb://localhost:27017/TodoApp');
+
+
+module.exports = {
+    mongoose: mongoose
+}
