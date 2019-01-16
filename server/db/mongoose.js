6bd845d9aa99f8d9af4f1e030951a85e2c7e6de0
Sagar Patil <sagarkp3@gmail.com>
2019-01-16 22:53:11 +0530

    changed in mongoose connection and some changes

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,14 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
-let db = {
-    localhost: 'mongodb://localhost:27017/TodoApp',
-    mlab: process.env.MONGOLAB_URI = 'mongodb://sagarkp3@gmail.com:Sagarkp3@ds237713.mlab.com:37713/todoapp'
-};
-mongoose.connect(db.mlab || db.localhost );
-
-
-module.exports = {
-    mongoose: mongoose
-}
+module.exports = { mongoose };
--- server/server.js
+++ server/server.js
@@ -7,7 +7,6 @@ var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
 
 var app = express();
-const host = '0.0.0.0';
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
@@ -50,7 +49,7 @@ app.get('/todos/:id', (req, res) => {
   });
 });
 
-app.listen(port, host, () => {
+app.listen(port, () => {
   console.log(`Started up at port ${port}`);
 });
 
